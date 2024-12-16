fff087abc8bb07cf91a3fb1edd64ccde9d792db0
hychaochao <hyz.chaochao@gmail.com>
2024-12-16 15:35:08 +0800

    modify results

--- run.sh
+++ run.sh
@@ -2,29 +2,51 @@
 OMP_NUM_THREADS=8 nohup srun \
  --partition=MoE \
  --mpi=pmi2 \
- --job-name=hyz \
+ --job-name=intern2 \
  -c 32 \
  -w SH-IDCA1404-10-140-54-89 \
  --ntasks-per-node=1 \
  --kill-on-bad-exit=1 \
  --quotatype=reserved \
 python generate_response.py \
---subject 'Coding' \
---strategy 'Direct' \
+--subject 'Physics' 'Chemistry' \
+--strategy 'CoT' \
+--rerun \
 --model_path '/mnt/petrelfs/share_data/quxiaoye/models/InternVL2-Llama3-76B' \
---output_path 'results/InternVL2_Coding_Direct.json' 1>logs/InternVL2_Coding_Direct.log 2>&1 &
-
+--output_path 'results/open-source/InternVL2_CoT.json' 1>logs/InternVL2_CoT.log 2>&1 &
 # python generate_response.py \
-# --subject 'Coding' \
-# --strategy 'Direct' \
+# --subject 'Physics' 'Chemistry' \
+# --strategy 'CoT' \
+# --rerun \
+# --model_path '/mnt/petrelfs/share_data/quxiaoye/models/InternVL2_5-78B' \
+# --output_path 'results/open-source/InternVL2_5_CoT.json' 1>logs/InternVL2_5_CoT.log 2>&1 &
+# python generate_response.py \
+# --subject 'Physics' 'Chemistry' \
+# --strategy 'CoT' \
+# --rerun \
 # --model_path '/mnt/petrelfs/share_data/quxiaoye/models/llava-onevision-qwen2-72b-ov-hf' \
-# --output_path 'results/Llava_Coding_Direct.json' 1>logs/Llava_Coding_Direct.log 2>&1 &
-
+# --output_path 'results/open-source/Llava_CoT.json' 1>logs/Llava_CoT.log 2>&1 &
 # python generate_response.py \
-# --subject 'Coding' \
-# --strategy 'Direct' \
+# --subject 'Physics' 'Chemistry' \
+# --rerun \
+# --strategy 'CoT' \
 # --model_path '/mnt/petrelfs/share_data/quxiaoye/models/Qwen2-VL-72B-Instruct' \
-# --output_path 'results/Qwen_Direct.json' 1>logs/Qwen_Coding_Direct.log 2>&1 &
+# --output_path 'results/open-source/Qwen_CoT.json' 1>logs/Qwen_CoT.log 2>&1 &
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
