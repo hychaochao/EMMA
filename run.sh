d62c198bd3c6a7aacd12e54d960a5466386ea8d9
hychaochao <hyz.chaochao@gmail.com>
2024-12-17 20:12:56 +0800

    update open-source-results

--- run.sh
+++ run.sh
@@ -4,34 +4,39 @@ OMP_NUM_THREADS=8 nohup srun \
  --mpi=pmi2 \
  --job-name=intern2 \
  -c 32 \
- -w SH-IDCA1404-10-140-54-89 \
+ -w SH-IDCA1404-10-140-54-102 \
  --ntasks-per-node=1 \
  --kill-on-bad-exit=1 \
  --quotatype=reserved \
 python generate_response.py \
 --subject 'Physics' 'Chemistry' \
---strategy 'CoT' \
+--strategy 'Direct' \
 --rerun \
 --model_path '/mnt/petrelfs/share_data/quxiaoye/models/InternVL2-Llama3-76B' \
---output_path 'results/open-source/InternVL2_CoT.json' 1>logs/InternVL2_CoT.log 2>&1 &
+--output_path 'results/open-source/InternVL2_Direct.json' 1>logs/InternVL2_Direct.log 2>&1 &
 # python generate_response.py \
 # --subject 'Physics' 'Chemistry' \
-# --strategy 'CoT' \
+# --strategy 'Direct' \
+# --rerun \
+# --model_path '/mnt/petrelfs/share_data/quxiaoye/models/Qwen2-VL-72B-Instruct' \
+# --output_path 'results/open-source/Qwen_Direct.json' 1>logs/Qwen_Direct.log 2>&1 &
+# python generate_response.py \
+# --subject 'Physics' 'Chemistry' \
+# --strategy 'Direct' \
 # --rerun \
 # --model_path '/mnt/petrelfs/share_data/quxiaoye/models/InternVL2_5-78B' \
-# --output_path 'results/open-source/InternVL2_5_CoT.json' 1>logs/InternVL2_5_CoT.log 2>&1 &
+# --output_path 'results/open-source/InternVL2_5_Direct.json' 1>logs/InternVL2_5_Direct.log 2>&1 &
 # python generate_response.py \
 # --subject 'Physics' 'Chemistry' \
-# --strategy 'CoT' \
+# --strategy 'Direct' \
 # --rerun \
 # --model_path '/mnt/petrelfs/share_data/quxiaoye/models/llava-onevision-qwen2-72b-ov-hf' \
-# --output_path 'results/open-source/Llava_CoT.json' 1>logs/Llava_CoT.log 2>&1 &
-# python generate_response.py \
-# --subject 'Physics' 'Chemistry' \
-# --rerun \
-# --strategy 'CoT' \
-# --model_path '/mnt/petrelfs/share_data/quxiaoye/models/Qwen2-VL-72B-Instruct' \
-# --output_path 'results/open-source/Qwen_CoT.json' 1>logs/Qwen_CoT.log 2>&1 &
+# --output_path 'results/open-source/Llava_Direct.json' 1>logs/Llava_Direct.log 2>&1 &
+
+
+
+
+
 
 
 
