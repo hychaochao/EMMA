c445737d3a32fa8a524ee3eee8082b19f5e2bf1a
hychaochao <hyz.chaochao@gmail.com>
2024-12-19 20:53:35 +0800

    add

--- scoring.py
+++ scoring.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = "4,5,6,7"
+os.environ['CUDA_VISIBLE_DEVICES'] = "0,1,2,3"
 import logging
 from tqdm import tqdm
 from datasets import load_dataset
